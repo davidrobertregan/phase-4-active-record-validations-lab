f9503e6f8af933c247922322533f2e57a7d96ece
David Regan <83370854+davidrobertregan@users.noreply.github.com>
2021-10-12 16:50:34 -0500

    commit tree c4342498b5bcba69ceb3b8ba48effd27f2521313

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,22 @@
 class Post < ApplicationRecord
+    validates :title, presence: true
+    validates :content, length: { minimum: 250 }
+    validates :summary, length: { maximum: 250 }
+    validates :category, inclusion: { in: ["Fiction", "Non-Fiction"]}
+    validate :click_baity
+
+
+    def click_baity
+        unless title.include?(/Won't Believe/i,/Secret/i,/Guess/i,/Top \d/i)
+            self.errors.add(:title, "This is spammy")
+        end
+    end
+
+# could not figure this out
+
+    # def click_baity
+    #     unless title.match?(/Won't Believe/i,/Secret/i,/Guess/i,/Top \d/i)
+    #         errors.add(:title, message: "Not click baity")
+    #     end
+    # end
 end
